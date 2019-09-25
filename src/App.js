a6834fd5e1a49765d3c366ee422ab3dabc9d3815
Joe Collins <JoeCollins@Charles-7.local>
2019-09-25 10:40:34 +0800

    spaghetticode

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <body class="class20">
+     <div><h1 class="class21">STACK ASSIGNMENT</h1></div>
+ <div class="class250">
+      <div class="class24">
+          <p class="26">Here, the USER (me in cartoon form) is distracted by their phonewhile walking. This is a problem I frequently  notice and exacerbate on my way to school. I personally usually browse Reddit or the NYT because they have the most interesting content.
+          </p>
+          <img src="https://cdn1.imggmi.com/uploads/2019/9/17/b7408401163f89bf488bc6338e68abbd-full.jpg" width="240px"></img>
+      </div>
+      <div class="class24">
+          <p class="26">The specific problem I identified was using the Reddit INTERFACE. This is an issue because the interface of Reddit leads the user to interesting content that all revolve around visual stimulation, meaning you can easily inadvertently ignore the dangers nearby.</p>
+          <img src="https://cdn1.imggmi.com/uploads/2019/9/17/fbc7c88c264676935f0dd94699ebc39b-full.png" width="240px"></img>
+      </div>
+      <div class="class24">
+          <p class="26">The ADDRESS of your phone and more critically, the ease  at which this address is  accessable means that people  are extremely tempted by the instant gratification that a strong cellular ecosystem allows for.</p>
+          <img src="https://cdn1.imggmi.com/uploads/2019/9/17/b32f354195fde1bcc0a6029738001092-full.jpg" width="300px"></img>
+      </div>
+      <div class="class24">
+              <p class="26">The CITY heightens this problem by providing a fertile network of connectivity and economic rationale for this cellular level. </p>
+              <img src="https://cdn1.imggmi.com/uploads/2019/9/17/7426c697d2b6c666d8ebdbae014bc7cd-full.jpg" width="350px"></img>
+      </div>
+      <div class="class24">
+              <p class="26">Reddit uses the AWS CLOUD to ensure that content is able to reach me quickly enough where I feel that if I have a few seconds of free time i'm still able to access content & people from all over the world without delay.</p>
+              <img src="https://cdn1.imggmi.com/uploads/2019/9/17/15c33ba6d8345d9ff898fe1223593097-full.png" width="200"></img>
+      </div>
+      <div class="class24">
+              <p class="26">Finally, AWS connects me in Shanghai back to everywhere else on EARTH, and since other people are the content creators on sites like Reddit, this connection is critical to me accidentally getting hit by an eleme driver in the morning.</p>
+              <img src="https://cdn1.imggmi.com/uploads/2019/9/17/bc58ab36a623b4bd76348fc8f3da6a9f-full.jpg" width="300px"></img>
+      </div>
+      </div>
+      </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
